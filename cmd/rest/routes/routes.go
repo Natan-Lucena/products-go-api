0b561ee6a2cc8704f8557390b7023c956b96c121
Natan Lucena <zerogamerptbr@gmail.com>
2025-04-05 22:41:40 -0300

    :sparkles: feat: enhance routing by adding authentication middleware and product creation endpoint

--- cmd/rest/routes/routes.go
+++ cmd/rest/routes/routes.go
@@ -1,10 +1,16 @@
 package rest
 
 import (
+	"github.com/Natan-Lucena/products-api/internal/infraestructure/factories"
+	"github.com/Natan-Lucena/products-api/internal/infraestructure/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func AppRoutes(router *gin.Engine) *gin.RouterGroup {
+	createProductController := factories.GetCreateProductController()
+
 	v1:= router.Group("/v1") 
+	v1.Use(middlewares.AuthMiddleware())
+	v1.POST("/products", createProductController.Handle)
 	return v1
 }
